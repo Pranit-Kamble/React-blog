36fa493ca9e75eb334a346855b0c23ecaf36fb24
Pranit-Kamble <kpranit0134@gmail.com>
2023-02-07 18:30:43 +0530

    commit

--- src/Components/LatestTravel/TravelClick.jsx
+++ src/Components/LatestTravel/TravelClick.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useLocation } from 'react-router-dom'
 import './TravelClick.css'
+import { Data } from '../CreateContext/contextdata'
+import { Link } from 'react-router-dom'
+import LatestFit from '../LatestFitness/LatestFit'
+
 
 const TravelClick = () => {
   const location=useLocation()
-  console.log(location)
+  const data=useContext(Data)
   const img=location.state.img
   const name=location.state.name
   const imdb=location.state.para
   return (
-    <div className='singletravel'>
+    <div>
+      <div className='singletravel'>
         <img className='singletrvelimg' src={img} alt="" />
         <h1 className='singletravelhead'>{name}</h1>
         <p className='singletravelpara'>{imdb}</p>
-        
     </div>
+    <h1 style={{textAlign:'center'}}>Anime</h1>
+      <div className='fitnesscon'>
+      <Link state={data.fitness.post1} to='/latesttravel'><LatestFit img={data.fitness.post1.img} name={data.fitness.post1.name} para={data.fitness.post1.para} /></Link>
+        <Link state={data.fitness.post2} to='/latesttravel'><LatestFit img={data.fitness.post2.img} name={data.fitness.post2.name} para={data.fitness.post2.para} /></Link>
+        <Link state={data.fitness.post3} to='/latesttravel'><LatestFit img={data.fitness.post3.img} name={data.fitness.post3.name} para={data.fitness.post3.para} /></Link>
+    </div>
+    </div>
+    
   )
 }
 
